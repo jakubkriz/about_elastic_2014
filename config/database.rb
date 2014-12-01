f139bd8ed1a0d536d8119b5ea41ff0db2ab111be
Tomáš Hrabal <fil.hrabal@gmail.com>
2014-12-01 08:53:00 +0100

    [MOD] Add log to repository

--- config/database.rb
+++
@@ -1,8 +0,0 @@
-# encoding: UTF-8
-require 'sqlite3'
-require 'sequel'
-require 'yaml'
-
-ENV["RACK_ENV"] ||= "development"
-database_config = YAML::load(File.read("config/database.yml"))
-DB = Sequel.connect(database_config[ENV["RACK_ENV"]])
